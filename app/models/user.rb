9ee0ed44c84868d7ee7ea613b17319a37df81937
ysktlv <k109tkms@gmail.com>
2020-08-07 21:56:32 +0900

    定数の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :birth_date, presence: true
-  password_format = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
-  validates :password, format: { with:password_format, message: "は英字と数字の両方を含めて設定してください" }
-  name_format = /\A[ぁ-んァ-ン一-龥]+\z/
-  validates :last_name, :first_name, presence: true, format: { with: name_format, message: "は全角文字を使用してください" }
-  name_reading_format = /\A[ァ-ン]+\z/
-  validates :last_name_reading, :first_name_reading, presence: true, format: { with: name_reading_format, message: "は全角カナを使用してください" }
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  VALID_NAME_READING_REGEX = /\A[ァ-ン]+\z/
+  
+  validates :nickname, :birth_date,
+    presence: true
+  validates :password,
+    format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字の両方を含めて設定してください" }
+  validates :last_name, :first_name,
+    presence: true,
+    format: { with: VALID_NAME_REGEX, message: "は全角文字を使用してください" }
+  validates :last_name_reading, :first_name_reading,
+    presence: true,
+    format: { with: VALID_NAME_READING_REGEX, message: "は全角カナを使用してください" }
 end
