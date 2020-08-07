c68e41b6cd77404cb28b9b7b9028c4fcdc1d626c
ysktlv <k109tkms@gmail.com>
2020-08-07 13:29:11 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birth_date])
+  end
 
   private
   
