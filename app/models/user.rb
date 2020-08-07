1ae3d5241d34add20597c850c66e6be4f5441b0f
ysktlv <k109tkms@gmail.com>
2020-08-07 21:45:16 +0900

    指摘箇所の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :birth_date, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "は英字と数字の両方を含めて設定してください" }
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字を使用してください" }
-  validates :last_name_reading, :first_name_reading, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "は全角カナを使用してください" }
+  password_format = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  validates :password, format: { with:password_format, message: "は英字と数字の両方を含めて設定してください" }
+  name_format = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates :last_name, :first_name, presence: true, format: { with: name_format, message: "は全角文字を使用してください" }
+  name_reading_format = /\A[ァ-ン]+\z/
+  validates :last_name_reading, :first_name_reading, presence: true, format: { with: name_reading_format, message: "は全角カナを使用してください" }
 end
