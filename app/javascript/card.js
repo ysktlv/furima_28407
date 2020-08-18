1fe79d6de3f18e4b286bb169b31675fd2a60a9a1
ysktlv <k109tkms@gmail.com>
2020-08-18 21:20:25 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,6 +13,7 @@ const pay = () => {
       exp_year: `20${formData.get("exp_year")}`,
     };
     Payjp.createToken(card, (status, response) => {
+      console.log(response);
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
