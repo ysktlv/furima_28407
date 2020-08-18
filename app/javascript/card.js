eaf6980897c13e840d0cfa374c05ea8fa90f94db
ysktlv <k109tkms@gmail.com>
2020-08-18 19:42:55 +0900

    指摘箇所の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,6 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
  
   const form = document.getElementById("charge-form");
-  console.log(form)
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formResult = document.getElementById("charge-form");
